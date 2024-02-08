486cfef244d7fa8fbe1c5578620568ca3f2edb2f
Kailash Nath <kailashnathashok@gmail.com>
2024-02-08 16:47:37 +0300

    making categories using set

--- 05-menu/starter/src/App.jsx
+++ 05-menu/starter/src/App.jsx
@@ -3,9 +3,15 @@ import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
 
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ["all", ...tempSet];
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
-
+  const [categories, setCategories] = useState(allCategories);
   return (
     <main>
       <section className="menu">
