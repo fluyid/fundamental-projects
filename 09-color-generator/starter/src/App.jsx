dc70cf86fcf575ee6510c31666d04344dbe83499
fluyid <kailashnathashok@gmail.com>
2024-02-24 15:29:51 +0300

    installed react toastify

--- 09-color-generator/starter/src/App.jsx
+++ 09-color-generator/starter/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ColourList from "./ColourList";
 import Form from "./Form";
 import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const [colors, setColors] = useState(new Values("#f15025").all(10));
@@ -9,6 +10,7 @@ const App = () => {
     <main>
       <Form />
       <ColourList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
