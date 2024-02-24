2a4f045370e285b4e93e007a5c357f046a80b635
fluyid <kailashnathashok@gmail.com>
2024-02-24 21:55:23 +0300

    used react toastify to show item added, removed and to enter an item message while a refresh happens

--- 10-grocery-bud/starter/src/App.jsx
+++ 10-grocery-bud/starter/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
+import { ToastContainer, toast } from "react-toastify";
 
 const getLocalStorage = () => {
   let list = localStorage.getItem("list");
@@ -28,11 +29,13 @@ const App = () => {
     const newItems = [...items, newItem];
     setItems(newItems);
     setLocalStorage(newItems);
+    toast.success("item added to the list");
   };
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
     setLocalStorage(newItems);
+    toast.success("item deleted");
   };
   const editItem = (itemId) => {
     const newItems = items.map((item) => {
@@ -47,6 +50,7 @@ const App = () => {
   };
   return (
     <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
       <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
