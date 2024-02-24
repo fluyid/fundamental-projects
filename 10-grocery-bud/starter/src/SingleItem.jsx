6e66b5f637dc373bd19c958d32a16ace1a1c7ea4
fluyid <kailashnathashok@gmail.com>
2024-02-24 21:19:43 +0300

    designed what a single item would look like

--- 10-grocery-bud/starter/src/SingleItem.jsx
+++ 10-grocery-bud/starter/src/SingleItem.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SingleItem = () => {
-  return <h2>SingleItem</h2>;
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: isChecked && "line-through",
+        }}
+      >
+        {item.name}
+      </p>
+      <button className="btn remove-btn" type="button">
+        delete
+      </button>
+    </div>
+  );
 };
 
 export default SingleItem;
