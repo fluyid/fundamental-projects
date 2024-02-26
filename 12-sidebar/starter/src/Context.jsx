53dc2a5b00c5e548bb9e434deb9b1b5d79096eb8
fluyid <kailashnathashok@gmail.com>
2024-02-26 20:15:39 +0300

    making the functions for open/close modal and sidebar

---
+++ 12-sidebar/starter/src/Context.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSiderbarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSideSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSiderbarOpen,
+        isModalOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSideSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
--- 12-sidebar/starter/src/Home.jsx
+++ 12-sidebar/starter/src/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useGlobalContext } from "./Context";
 
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+
   return <div>Home</div>;
 };
 
