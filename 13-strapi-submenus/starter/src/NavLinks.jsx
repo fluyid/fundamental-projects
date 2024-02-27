b5c830434e3f3d29bd4f02d81a3d7510f50a6b06
fluyid <kailashnathashok@gmail.com>
2024-02-27 13:53:58 +0300

    adding pageId to context and navlinks

--- 13-strapi-submenus/starter/src/NavLinks.jsx
+++ 13-strapi-submenus/starter/src/NavLinks.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import sublinks from "./data";
+import { useGlobalContext } from "./Context";
 
 const NavLinks = () => {
+  const { setPageId } = useGlobalContext();
   return (
     <div className="nav-links">
       {sublinks.map((item) => {
         const { page, pageId } = item;
         return (
-          <button key={pageId} className="nav-link">
+          <button
+            key={pageId}
+            className="nav-link"
+            onMouseEnter={() => setPageId(pageId)}
+          >
             {page}
           </button>
         );
