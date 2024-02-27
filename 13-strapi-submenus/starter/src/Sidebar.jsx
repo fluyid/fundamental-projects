93e8d3bf85b8e82c658de9d6330a29fa72b41cfb
fluyid <kailashnathashok@gmail.com>
2024-02-27 13:27:10 +0300

    making the sidebar

--- 13-strapi-submenus/starter/src/Sidebar.jsx
+++ 13-strapi-submenus/starter/src/Sidebar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useGlobalContext } from "./Context";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
 
 const Sidebar = () => {
-  return <h2>Sidebar</h2>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item) => {
+            const { links, page, pageId } = item;
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link;
+                    return (
+                      <a key={id} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
