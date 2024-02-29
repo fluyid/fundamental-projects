e4119a9788220e484112e587a3df564b462db769
fluyid <kailashnathashok@gmail.com>
2024-02-29 14:48:00 +0300

    adding the increase functionality

--- 14-cart/starter/src/Context.jsx
+++ 14-cart/starter/src/Context.jsx
@@ -31,8 +31,12 @@ export const AppProvider = ({ children }) => {
     dispatch({ type: REMOVE, payload: { id } });
   };
 
+  const increase = (id) => {
+    dispatch({ type: INCREASE, payload: { id } });
+  };
+
   return (
-    <AppContext.Provider value={{ ...state, clearCart, remove }}>
+    <AppContext.Provider value={{ ...state, clearCart, remove, increase }}>
       {children}
     </AppContext.Provider>
   );
