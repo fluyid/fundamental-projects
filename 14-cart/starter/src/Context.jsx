250570f530d36d968b111f228a296c44e4c90a3c
fluyid <kailashnathashok@gmail.com>
2024-02-29 13:23:55 +0300

    Using new Map() to make the app more efficient

---
+++ 14-cart/starter/src/Context.jsx
@@ -0,0 +1,31 @@
+import { createContext } from "react";
+import { useReducer } from "react";
+import { useContext } from "react";
+import reducer from "./Reducer";
+import cartItems from "./data";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./Actions";
+
+const AppContext = createContext();
+
+const initalState = {
+  loading: false,
+  cart: new Map(cartItems.map((item) => [item.id, item])),
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initalState);
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
---
+++ 14-cart/starter/src/Reducer.js
@@ -0,0 +1,14 @@
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./Actions";
+
+const reducer = (state, action) => {
+  return state;
+};
+
+export default reducer;
